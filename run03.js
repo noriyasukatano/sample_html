e602fbf5bd7b74c50b820b08e1a76776e8232a04
sumple_html <nori559259@gmail.com>
2016-12-29 14:45:28 +0900

    add runjs

---
+++ run03.js
@@ -0,0 +1,102 @@
+
+
+
+
+$.ajax({
+	var hatenaRss = http://noriyasu-katano.hatenablog.com/rss
+	url:hatenaRss;
+	success: function(){
+	alert(hatenaRss);
+
+	}
+});
+
+/*$.ajax({
+//var htmlstr = "";
+//htmlstr += '<div>';
+//htmlstr += '<ul>';
+
+
+
+/*for (var k=0; k<FA.length; k++) {
+
+	var rss_url = FA[k];
+	$.get(rss_url, function(data) {
+		$(data).find("item").each(function (i) { // or "entry"
+		var el = $(this);
+
+
+
+//		htmlstr += '<a href="' + el.find("link").text() + '" title="' + el.find("title").text() + '" rel="nofollow" target="_blank">' + el.find("title").text() + ' - ' + el.find("category").text() + '</a>';
+//		htmlstr += '</li>';
+	});
+});*/
+
+
+//htmlstr += '</ul>';
+//htmlstr += '</div>';
+
+//var container = document.getElementById("aq_feed");
+//container.innerHTML = htmlstr;
+
+//});
+
+
+/* google.load("feeds","1");
+var FA = new Array //表示させたいRSSフィードのURLを配列に代入
+("http://feedblog.ameba.jp/rss/ameblo/asakaza1224/rss20.xml",
+"http://tequila73.at.webry.info/rss/index.rdf",
+"http://nke.seesaa.net/index20.rdf");
+
+function initialize() {
+	var feedsArr = new Array();
+	var numEntr = 3; //各RSSフィードの取得件数
+	var container = document.getElementById("feed");
+	var cnt = FA.length;
+
+	for (var k=0; k<FA.length; k++) {
+		var feed = new google.feeds.Feed(FA[k]);
+		feed.setNumEntries(numEntr);
+		feed.setResultFormat(google.feeds.Feed.JSON_FORMAT);
+		feed.load(function(result) {
+			if (!result.error) {
+				for (var i = 0; i < result.feed.entries.length; i++) {
+					var entry = result.feed.entries[i];
+					var attributes = ["title", "link", "publishedDate", "contentSnippet"];
+					var ind = feedsArr.length;
+					feedsArr[ind] = new Array();
+					feedsArr[ind][0] = Date.parse(entry[attributes[2]]); // 日付順にソート
+					feedsArr[ind][1] = entry[attributes[1]]; // リンク
+					feedsArr[ind][2] = entry[attributes[2]]; // 更新日時
+					feedsArr[ind][3] = entry[attributes[3]]; // 記事本文
+					feedsArr[ind][4] = entry[attributes[0]]; // 記事タイトル
+					feedsArr[ind][5] = result.feed.title; // サイトタイトル
+				}
+			}
+
+
+			cnt--;
+			if (cnt == 0) {
+				feedsArr.sort();
+				feedsArr.reverse();
+				for (var j = 0; j < feedsArr.length;  j++) {
+					var aE = document.createElement("A");
+					var h3 = document.createElement("H3");
+					var p = document.createElement("P");
+					var spanD = document.createElement("SPAN");
+					aE.href=aE.title=feedsArr[j][1];
+					aE.appendChild(document.createTextNode(feedsArr[j][4]));
+					h3.appendChild(aE);
+					spanD.appendChild(document.createTextNode(feedsArr[j][2]));
+					spanD.appendChild(document.createTextNode(" ("+feedsArr[j][5]+")"));
+					p.appendChild(document.createTextNode(feedsArr[j][3]));
+					p.appendChild(spanD);
+					container.appendChild(h3);
+					container.appendChild(p);
+				}
+			}
+		});
+	}
+}
+google.setOnLoadCallback(initialize);
+*/
